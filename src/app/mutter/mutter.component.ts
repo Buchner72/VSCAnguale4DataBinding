cdb9df65f97f1fd7db41252bd1e74192dc6b3ff6
Buchner72 <franz.buchner@gmx.at>
2017-08-08 13:39:52 +0200

    Beispiel commit

--- src/app/mutter/mutter.component.ts
+++ src/app/mutter/mutter.component.ts
@@ -4,15 +4,51 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-mutter',
   template: `
        <div class="bs-docs-example"> <!--styles.css-->
-       app-mutter!
-        <app-tochter></app-tochter>
+         <h1>app-mutter!</h1>
+        <ng-content></ng-content>  <!--Platzhalter für übernehme aus app.component.html-->
+        <hr>
+        <p [innerText]="aString"></p>
+        <div [ngClass]="{'roter-rahmen': IsRahmen}">
+          Rahmen verschwindet in 3 Sekunden, mittels Property Binding
+        </div>
+        <hr>
+        <button (click)="IsRahmen=true">Rahmen wieder aktiveren(Event)</button>
+         <button (click)="onClick()">Rahmen wieder aktiveren(Event -Methode) für 3 Sek.</button>
+         <hr>
+         <p>{{aStringTochter}}</p>
+         <p>{{tstring}}</p>
+         <hr>
+        <app-tochter
+         (clicked)="aStringTochter =$event"
+         (clicked2)="tstring =$event"
+         [aString]="'String mittels Property Binding von Mutter zur Tochter->@Input() übergeben'">
+        </app-tochter>
       </div>
   `,
-  styles: []
+  styles: [`
+  .roter-rahmen{
+    border: 1px solid red;
+  }
+  `]
 })
 export class MutterComponent implements OnInit {
+  aString = 'Ich bin eine string [Property Binding]';
+  aStringTochter = 'Ich wurde noch nicht verändert: erst In Tochter -> Event an Mutter übergeben klicken!';
+  IsRahmen = true;
+  tstring = 'leer';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.IsRahmen = false;
+    }, 3000);
+  }
+
+  onClick(){
+    this.IsRahmen = true;
+     setTimeout(() => {
+      this.IsRahmen = false;
+    }, 3000);
+  }
 
   ngOnInit() {
   }
--- src/app/tochter/tochter.component.ts
+++ src/app/tochter/tochter.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-tochter',
   template: `
      <div class="bs-docs-example"> <!--styles.css-->
-      app-tochter!
+      <h1>app-tochter!</h1>
+      <p>{{aString}}</p>
+      <div>
+      <button (click)="onClick()">Event an Mutter übergeben!</button>
+      </div>
+      <hr>
+        <input type="text" [value]="tstring" (keyup)="tstring = input.value" #input>
+        Two-Way-Binding selber geschaffen!
+        <hr>
+        <input type="text" [(ngModel)]="tstring">
+        Two-Way-Binding mit [(ngModel)] : import FormsModule in app-module nicht vergessen
    </div>
   `,
   styles: []
 })
 export class TochterComponent implements OnInit {
+ @Input()  aString: string;
+ @Output() clicked = new EventEmitter<string>();
+ @Output() clicked2 = new EventEmitter<string>();
+ tstring = 'start';
+ // Ermöglicht es eigene events zu starten -> @Output() damit von aussen reagiert werden kann
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClick(){
+    this.clicked.emit('Event aus Tochter übernommen'); // Neues event los schicken!
+     this.clicked2.emit(this.tstring); // Neues event los schicken!
+  }
 }
